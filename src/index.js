a0dda479dbe19447a043e407d5358bedc043d2ad
Drew Peterson and Noah Horwitz <drew@>
2019-11-13 12:00:51 -0800

    update front and back end logic, test jest suite.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // Business (or back-end) logic:
 
-// import { Triangle } from './triangle-logic.js';
+import { Triangle } from './triangle-logic.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
 
 
 
@@ -16,23 +17,12 @@ $(document).ready(function() {
     var side1 = parseInt($("#side1").val());
     var side2 = parseInt($("#side2").val());
     var side3 = parseInt($("#side3").val());
-    // console.log(side1);
-    // console.log(side2);
-    // console.log(side3);
 
-    if (side1 === side2 && side2 === side3){
-      $("#result").text("an equilateral triangle.");
-    }
-    else if (side1 === side2 || side2 === side3 || side3 === side1){
-      $("#result").text("an isosceles triangle.");
-    }
-    else if ((side1 + side2 <= side3 ) || (side2 + side3 <= side1) || (side1 + side3 <= side2)){
-      $("#result").text("something other than a triangle.");
-    }
-    else{
-      $("#result").text("a scalene triangle.");
-    }
+    var triangle = new Triangle(side1, side2 , side3);
+    var result = triangle.checkType();
+
     $(".results").show();
+    $("#result").text(result);
 
     event.preventDefault();
   });
